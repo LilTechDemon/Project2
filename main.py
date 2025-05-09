aacd2af3812e20d70078859d75089becaf97a5a0
LilTechDemon <liltechdemon@gmail.com>
2025-05-08 22:31:03 -0500

    forgot to save after adding credit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from logic import SnakeGame
 
+# base code/ idea from https://www.geeksforgeeks.org/snake-game-using-tkinter-python/
+
 def main():
     root = tk.Tk()
     root.title("Snake Game")
